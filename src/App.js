686f79c89f5f1c6e6431f76a8415f3345e4a38a4
Rob <93198775+robdrawspictures@users.noreply.github.com>
2022-04-06 15:27:48 +0100

    Crit functions added, ascension added, minor styling applied.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import Assets from './components/Assets';
 import Enemy from './components/Enemy';
-import './App.css';
+// import './App.css';
 
 function App() {
 
   const [level, setLevel] = useState(1);
-  const [dmg, setDMG] = useState(10 * level);
+  const [lambentOrb, setLambentOrb] = useState(0);
+  const [radiantOrb, setRadiantOrb] = useState(0);
+  const [dmg, setDMG] = useState(10 * (level + radiantOrb));
   const [critDMG, setCritDMG] = useState(2);
   const [critChance, setCritChance] = useState(9);
   const [xp, setXP] = useState(0);
   const [nextLevel, setNextLevel] = useState(100);
   const [killCount, setKillCount] = useState(0);
-  const [lambentOrb, setLambentOrb] = useState(0);
-  const [radiantOrb, setRadiantOrb] = useState(0);
+  const [bossFight, setBossFight] = useState(false);
   
   let monsters = [
     {"name": "Slime", "hp": 20, "maxHP": 20, "src": Assets.Images.Slime},
@@ -41,6 +42,10 @@ function App() {
     return Math.floor(Math.random() * ceiling);
   }
 
+  const getCritRoll = () => {
+    return Math.floor(Math.random() * critChance);
+  }
+
   let currentMonster = monsters[monsterRandomiser()];
 
   useEffect(() => {
@@ -56,38 +61,50 @@ function App() {
   });
 
   const handleAttack = () => {
+    let critRoll = getCritRoll();
+    let critHit = dmg * critDMG;
+    if(critRoll === 0 || critRoll === 1 || critRoll === 2){
+      setNewMonster(previousState => {
+        let newHp = currentMonster.hp - critHit
+        return {...previousState, hp: newHp}
+      })
+      currentMonster = newMonster;
+      console.log(critHit + "CRIT");
+    }
       setNewMonster(previousState => {
         let newHp = currentMonster.hp - dmg
         return {...previousState, hp: newHp}
       })
       currentMonster = newMonster;
       console.log(dmg + "DMG");
-      console.log("I am in handle attack and the current monstert is {}", currentMonster);
-    console.log(getMonsterHP())
+      // console.log("I am in handle attack and the current monstert is {}", currentMonster);
     }
 
   const handleRespawn = () => {
       if(killCount === 5){
         setNewMonster(bosses[0]);
         setKillCount(0);
+        setBossFight(true);
       } else {
       currentMonster = monsters[monsterRandomiser()];
       setNewMonster(currentMonster);
-      console.log("I am in handle respawn and the current monstert is {}", currentMonster);
+      // console.log("I am in handle respawn and the current monstert is {}", currentMonster);
       setKillCount(killCount + 1);
     }
       console.log(killCount);
       setXP(xp + (10 * (1 + xpModifier())));
       checkXP();
-      console.log('RESPAWN GET')
+      if(bossFight){
+        setLambentOrb(lambentOrb + 1);
+        setBossFight(false);
+      }
+      // console.log('RESPAWN GET')
     }
 
   const checkXP = () => {
     if(xp >= nextLevel){
       handleLevelUp();
     } 
-    let toNextLevel = nextLevel - xp;
-    console.log(toNextLevel + 'xp to next level.')
   }
 
   const checkHP = (currentMonster) => {
@@ -105,8 +122,6 @@ function App() {
     let currentNextLevel = nextLevel;
     let newNextLevel = currentNextLevel * 2;
     setNextLevel(newNextLevel);
-    console.log(level);
-    console.log(nextLevel);
     console.log('LEVEL UP')
   }
 
@@ -143,20 +158,55 @@ function App() {
     }
     return level / 10;
   }
+
+  const handleAscension = () => {
+    setLevel(1);
+    setXP(0);
+    setNextLevel(100);
+    let newRadiantOrb = radiantOrb + lambentOrb;
+    setRadiantOrb(newRadiantOrb);
+    setLambentOrb(0);
+  }
+
+  const handleCritChanceUpgrade = () => {
+    if(critChance > 2){
+      setCritChance(critChance - 1);
+    }
+  }
+
+  const handleCritDMGUpgrade = () => {
+    if(critDMG < 10){
+      setCritDMG(critDMG + 1);
+    }
+  }
   
 
   return (
     <div className="game-container">
-      <Enemy name={getMonsterName()} src={getMonsterSRC()} handleAttack={handleAttack} handleRespawn={handleRespawn} hp={getMonsterHP()} maxHP={getMonsterMaxHP()}/>
-      {/* <p>{monsters[0].name}</p>
-      <img src={monsters[0].src} alt="monstarrr"/>
-      <p>HP: {monsters[0].hp}/{monsters[0].maxHP}</p> */}
-      <h1>Lv.{level}</h1>
-      <h3>Current XP: {xp}</h3>
-      <h3>To Next Level: {calculateNextLevel()}</h3>
-      <h2>Kill Count: {killCount}</h2>
-      <button onClick={handleLevelUp}>LEVEL UP</button>
-      <button onClick={handleRespawn}>RESPAWN</button>
+      <div className='game-window'>
+        <div className="enemy-container">
+        <Enemy name={getMonsterName()} src={getMonsterSRC()} handleAttack={handleAttack} handleRespawn={handleRespawn} hp={getMonsterHP()} maxHP={getMonsterMaxHP()}/>
+        {/* <p>{monsters[0].name}</p>
+        <img src={monsters[0].src} alt="monstarrr"/>
+        <p>HP: {monsters[0].hp}/{monsters[0].maxHP}</p> */}
+        </div>
+        <div className='stat-container'>
+          <h1>---Stats---</h1>
+          <h2>Lv.{level}</h2>
+          <h3>Current XP: {xp}</h3>
+          <h3>To Next Level: {calculateNextLevel()}</h3>
+          <h3>Kill Count: {killCount}</h3>
+          <h4>Lambent Orbs: {lambentOrb}</h4>
+          <h4>Radiant Orbs: {radiantOrb}</h4>
+        </div>
+      </div>
+      <div className='button-container'>
+        <button onClick={handleLevelUp}>LEVEL UP</button>
+        <button onClick={handleRespawn}>RESPAWN</button>
+        <button onClick={handleAscension}>ASCEND</button>
+        <button onClick={handleCritChanceUpgrade}>Crit % UP</button>
+        <button onClick={handleCritDMGUpgrade}>Crit DMG UP</button>
+      </div>
     </div>
   );
 }
--- src/components/Enemy.js
+++ src/components/Enemy.js
@@ -3,16 +3,16 @@ function Enemy(props){
     function handleAttack(){
         if(props.hp > 0){
             props.handleAttack();
-            console.log("I am in Enemy and I am handling attack")
-            console.log("My hp is, {}", props.hp)
+            // console.log("I am in Enemy and I am handling attack")
+            // console.log("My hp is, {}", props.hp)
         } else {
             props.handleRespawn();
-            console.log('RESPAWN A GO GO BABY')
+            // console.log('RESPAWN A GO GO BABY')
         }
     }
 
     return (
-        <div className="enemy-container">
+        <div className="enemy">
             <p>{props.name}</p>
             <img className="enemy" src={props.src} alt="monster" onClick={() => handleAttack()}/>
             <p>HP: {props.hp}/{props.maxHP}</p>
